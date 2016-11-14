f0bbaecd0d2f2081bc18ba500ca1cd920aba1945
ktymann <karsten_michael.tymann@fh-bielefeld.de>
2016-11-14 22:57:14 +0100

    first mockups with angular

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Karsten on 08.11.2016.
+ */
+angular.module('tinytask', ['ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/', { templateUrl: 'components/login.html'})
+            .when('/register', { templateUrl: 'components/register.html'})
+            .when('/start', {templateUrl: 'components/start.html'})
+            .otherwise({ redirectTo: '/'});
+    })
+
+    .controller('LoginCtrl', function($scope, $http){
+        $http.get('welcome.json').then(function(welcomeResponse){
+            $scope.welcome = welcomeResponse.data;
+        });
+    })
+
+    .controller('StartCtrl', function($scope, $http){
+        $http.get('mytasks.json').then(function(mytasksResponse){
+            $scope.mytasks = mytasksResponse.data;
+        })
+    });
