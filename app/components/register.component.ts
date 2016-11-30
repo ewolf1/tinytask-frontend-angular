a0d42e34b3a1e1363dde550d71abba61e5a2b5e3
ktymann <karsten_michael.tymann@fh-bielefeld.de>
2016-11-30 10:55:25 +0100

    Various

--- app/components/register.component.ts
+++ app/components/register.component.ts
@@ -2,25 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  
-<img src="components/images/test.png" id="logo" class="img-thumbnail" >
-
-<div id="loginform">
-    <h2>Registrieren</h2>
-    <form action="#/">
-        Username: <input name="Username" type="text" ng-model="username">
-        email: <input name="email" type="email" ng-model="email">
-        <br>
-
-        Kennwort: <input name="Kennwort" type="text" ng-model="password">
-        Kennwort bestätigen: <input name="Kennwort" type="text" ng-model="password">
-        <input name="Jetzt registrieren" type="submit" >
-    </form>
-
-</div>
-  
-  `,
+  templateUrl: 'app/components/views/register.html',
+  styleUrls: ['app/components/views/styles/login.css'],
 })
 export class RegisterComponent  { 
 }
