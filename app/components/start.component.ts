f762262846f4374e29ecaa81f809e86edb4a9cc1
ktymann <karsten_michael.tymann@fh-bielefeld.de>
2016-12-08 15:02:13 +0100

    Add LogOut Function to Start-Screen

--- app/components/start.component.ts
+++ app/components/start.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Auth} from './service/auth.service';
+import {Inject} from '@angular/core';
+import { Http,Response } from '@angular/http';
+import {Headers, RequestOptions} from '@angular/http';
+import {Observable}     from 'rxjs/Observable';import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/components/views/start.html',
+  providers: [Auth],
   styleUrls: ['app/components/views/styles/login.css']
 })
 export class StartComponent  { 
-
+	constructor(private auth:Auth){
+		this.checkForLogin();
+	}
+	
+	private checkForLogin(){
+		console.log("Checking for a valid login...");
+		if(!localStorage.getItem("id_token"))
+			window.location.href = "";
+	}
  }
